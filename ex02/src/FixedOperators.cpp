5e6b92158d8a7ef070e9445c7c5095229f91e6a2
Leon David Zipp <lzipp@c4r3s8.clusters.forty2.fr>
2024-05-24 15:54:00 +0200

    added all operators, not tested yet

--- ex02/src/FixedOperators.cpp
+++ ex02/src/FixedOperators.cpp
@@ -10,53 +10,57 @@ bool Fixed::operator>(const Fixed& other) {
 }
 
 bool Fixed::operator<(const Fixed& other) {
-    return this->getRawBits() > other.getRawBits();
+    return this->getRawBits() < other.getRawBits();
 }
 
 bool Fixed::operator>=(const Fixed& other) {
-    return this->getRawBits() > other.getRawBits();
+    return this->getRawBits() >= other.getRawBits();
 }
 
 bool Fixed::operator<=(const Fixed& other) {
-    return this->getRawBits() > other.getRawBits();
+    return this->getRawBits() <= other.getRawBits();
 }
 
 bool Fixed::operator==(const Fixed& other) {
-    return this->getRawBits() > other.getRawBits();
+    return this->getRawBits() == other.getRawBits();
 }
 
 bool Fixed::operator!=(const Fixed& other) {
-    return this->getRawBits() > other.getRawBits();
+    return this->getRawBits() != other.getRawBits();
 }
 
 Fixed Fixed::operator+(const Fixed& other) const {
-    return this->getRawBits() > other.getRawBits();
+    return this->getRawBits() + other.getRawBits();
 }
 
 Fixed Fixed::operator-(const Fixed& other) const {
-
+    return this->getRawBits() - other.getRawBits();
 }
 
 Fixed Fixed::operator*(const Fixed& other) const {
-
+    return this->getRawBits() * other.getRawBits();
 }
 
 Fixed Fixed::operator/(const Fixed& other) const {
-
+    return this->getRawBits() / other.getRawBits();
 }
 
 Fixed& Fixed::operator++(void) {
-
+    this->setRawBits(this->getRawBits() + 1);
 }
 
 Fixed Fixed::operator++(int) {
-
+    Fixed temp(*this);
+    this->setRawBits(this->getRawBits() + 1);
+    return temp;
 }
 
 Fixed& Fixed::operator--(void) {
-
+    this->setRawBits(this->getRawBits() - 1);
 }
 
 Fixed Fixed::operator--(int) {
-
+    Fixed temp(*this);
+    this->setRawBits(this->getRawBits() - 1);
+    return temp;
 }
