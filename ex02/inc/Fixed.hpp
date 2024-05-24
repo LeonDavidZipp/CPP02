0d161e93aac36a8c1e9114a1b3b2656056c457df
Leon David Zipp <lzipp@c4r3s8.clusters.forty2.fr>
2024-05-24 14:50:33 +0200

    added Fixed && adjusted

---
+++ ex02/inc/Fixed.hpp
@@ -0,0 +1,54 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+    public:
+        Fixed(void);
+        Fixed(const int val);
+        Fixed(const float val);
+        Fixed(const Fixed& fixed);
+        ~Fixed(void);
+
+        int getRawBits(void) const;
+        void voidsetRawBits(int const raw);
+
+        int toInt(void) const;
+        float toFloat(void) const;
+
+        Fixed& operator=(const Fixed& otherFixed);
+
+        bool operator>(const Fixed& otherFixed);
+        bool operator<(const Fixed& otherFixed);
+        bool operator>=(const Fixed& otherFixed);
+        bool operator<=(const Fixed& otherFixed);
+        bool operator==(const Fixed& otherFixed);
+        bool operator!=(const Fixed& otherFixed);
+
+        Fixed	operator+(Fixed const &other) const;
+        Fixed	operator-(Fixed const &other) const;
+        Fixed	operator*(Fixed const &other) const;
+        Fixed	operator/(Fixed const &other) const;
+
+        Fixed&	operator++(void);
+        Fixed	operator++(int);
+        Fixed&	operator--(void);
+        Fixed	operator--(int);
+
+        const static Fixed& min(Fixed const &fixed1, Fixed const &fixed2);
+        static Fixed& min(Fixed& fixed1, Fixed& fixed2);
+
+        const static Fixed& max(Fixed const &fixed1, Fixed const &fixed2);
+        static Fixed& max(Fixed& fixed1, Fixed& fixed2);
+
+    private:
+        int val;
+        static const int fractBits;
+};
+
+std::ostream& operator<<(std::ostream &os, const Fixed& fixed);
+
+#endif
