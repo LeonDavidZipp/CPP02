1ad6df9b1333b7cf6ae7e36b4715fac7971f5da2
LeondavidZipp <leondavidzipp@gmx.de>
2024-09-10 08:28:34 +0000

    adjusted standard and tests

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -4,12 +4,22 @@ int main( void ) {
     Fixed a;
     Fixed b( a );
     Fixed c;
+	Fixed d = a;
 
     c = b;
 
+    std::cout << "--------------------------------" << std::endl;
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
+    std::cout << d.getRawBits() << std::endl;
+
+    std::cout << "--------------------------------" << std::endl;
+    a.setRawBits(42);
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << d.getRawBits() << std::endl;
+    std::cout << "--------------------------------" << std::endl;
 
     return 0;
 }
